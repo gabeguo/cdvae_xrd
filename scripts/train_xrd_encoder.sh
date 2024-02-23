472168394553807e134a9a8c249cfce06f3fa42b
gabeguo <gzg2104@columbia.edu>
2024-02-23 18:48:49 -0500

    training XRD encoder

--- scripts/train_xrd_encoder.sh
+++ scripts/train_xrd_encoder.sh
@@ -1,20 +1,35 @@
 python train_xrd_encoder.py \
-    --epochs 200 \
+    --epochs 100 \
     --batch_size 256 \
     --model_path /home/gabeguo/hydra/singlerun/2024-02-17/perov \
     --data_dir /home/gabeguo/cdvae/data/perov_5 \
     --lr 1e-4
 
-# python train_xrd_encoder.py \
-#     --epochs 50 \
-#     --batch_size 256 \
-#     --model_path /home/gabeguo/hydra/singlerun/2024-02-16/mp_20 \
-#     --data_dir /home/gabeguo/cdvae/data/mp_20 \
-#     --lr 1e-5
+python scripts/evaluate.py \
+    --model_path /home/gabeguo/hydra/singlerun/2024-02-17/perov \
+    --tasks recon \
+    --xrd
 
-# python train_xrd_encoder.py \
-#     --epochs 50 \
-#     --batch_size 256 \
-#     --model_path /home/gabeguo/hydra/singlerun/2024-02-16/carbon \
-#     --data_dir /home/gabeguo/cdvae/data/carbon_24 \
-#     --lr 1e-5
+python train_xrd_encoder.py \
+    --epochs 100 \
+    --batch_size 256 \
+    --model_path /home/gabeguo/hydra/singlerun/2024-02-16/mp_20 \
+    --data_dir /home/gabeguo/cdvae/data/mp_20 \
+    --lr 1e-4
+
+python scripts/evaluate.py \
+    --model_path /home/gabeguo/hydra/singlerun/2024-02-16/mp_20 \
+    --tasks recon \
+    --xrd
+
+python train_xrd_encoder.py \
+    --epochs 100 \
+    --batch_size 256 \
+    --model_path /home/gabeguo/hydra/singlerun/2024-02-16/carbon \
+    --data_dir /home/gabeguo/cdvae/data/carbon_24 \
+    --lr 1e-4
+
+python scripts/evaluate.py \
+    --model_path /home/gabeguo/hydra/singlerun/2024-02-16/carbon \
+    --tasks recon \
+    --xrd
