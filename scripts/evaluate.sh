7b933376734bc1e9d97518b53130d14453c3cdd3
gabeguo <gzg2104@columbia.edu>
2024-02-26 00:19:09 -0500

    added xrd option to evaluate

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -3,10 +3,13 @@ python -W ignore evaluate.py \
     --model_path /home/gabeguo/hydra/singlerun/2024-02-16/mp_20 \
     --tasks recon \
     --label xrd_threeSamples \
-    --num_evals 3
+    --num_evals 3 \
+    --xrd
 # forcing composition
 python -W ignore evaluate.py \
     --model_path /home/gabeguo/hydra/singlerun/2024-02-16/mp_20 \
     --tasks recon \
     --label xrd_givenComposition \
-    --force_atom_types
+    --force_num_atoms \
+    --force_atom_types \
+    --xrd
