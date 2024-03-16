f2babadbe28bdde49d3d70ba270b599637d4b602
Gabe Guo <gzg2104@columbia.edu>
2024-03-15 20:52:52 -0400

    added increased l2 penalty

--- scripts/conditional_generation.sh
+++ scripts/conditional_generation.sh
@@ -62,3 +62,16 @@ python -W ignore conditional_generation.py \
     --num_atom_lambda 0 \
     --composition_lambda 0.1 \
     --l1_loss
+
+python -W ignore conditional_generation.py \
+    --model_path /home/gabeguo/hydra/singlerun/2024-03-12/mp_20 \
+    --num_starting_points 150 \
+    --lr 0.1 \
+    --min_lr 0.001 \
+    --l2_penalty 2e-3 \
+    --num_tested_materials 20 \
+    --label mp_20_l2penalty2e-3 \
+    --num_gradient_steps 5000 \
+    --num_atom_lambda 0.1 \
+    --composition_lambda 0.1 \
+    --l1_loss
