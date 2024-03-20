636203503929bcf550033fcdf19c14c48f2af3f4
Gabe Guo <gzg2104@columbia.edu>
2024-03-20 12:23:49 -0400

    conditional generation

--- scripts/conditional_generation.sh
+++ scripts/conditional_generation.sh
@@ -1,90 +1,39 @@
 python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/hydra/singlerun/2024-03-12/mp_20 \
+    --model_path /home/gabeguo/hydra/singlerun/2024-03-17/mp_20_vaeBeta05_lr5e-4 \
     --num_starting_points 150 \
     --lr 0.1 \
     --min_lr 0.001 \
     --l2_penalty 5e-5 \
     --num_tested_materials 20 \
-    --label mp_20_tryCompositionInfo \
+    --label kldBeta_0_05_mp_20_l2_5e-5 \
     --num_gradient_steps 5000 \
     --num_atom_lambda 0.1 \
     --composition_lambda 0.1 \
     --l1_loss
 
 python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/hydra/singlerun/2024-03-12/mp_20 \
+    --model_path /home/gabeguo/hydra/singlerun/2024-03-17/mp_20_vaeBeta05_lr5e-4 \
     --num_starting_points 150 \
     --lr 0.1 \
     --min_lr 0.001 \
-    --l2_penalty 5e-5 \
+    --l2_penalty 1e-3 \
     --num_tested_materials 20 \
-    --label mp_20_comp1e2 \
-    --num_gradient_steps 5000 \
-    --num_atom_lambda 0.1 \
-    --composition_lambda 1e2 \
-    --l1_loss
-
-python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/hydra/singlerun/2024-03-12/mp_20 \
-    --num_starting_points 150 \
-    --lr 0.1 \
-    --min_lr 0.001 \
-    --l2_penalty 5e-5 \
-    --num_tested_materials 20 \
-    --label mp_20_comp1e-4 \
-    --num_gradient_steps 5000 \
-    --num_atom_lambda 0.1 \
-    --composition_lambda 1e-4 \
-    --l1_loss
-
-python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/hydra/singlerun/2024-03-12/mp_20 \
-    --num_starting_points 150 \
-    --lr 0.1 \
-    --min_lr 0.001 \
-    --l2_penalty 5e-5 \
-    --num_tested_materials 20 \
-    --label mp_20_noCompositionOpt \
-    --num_gradient_steps 5000 \
-    --num_atom_lambda 0.1 \
-    --composition_lambda 0 \
-    --l1_loss
-
-python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/hydra/singlerun/2024-03-12/mp_20 \
-    --num_starting_points 150 \
-    --lr 0.1 \
-    --min_lr 0.001 \
-    --l2_penalty 5e-5 \
-    --num_tested_materials 20 \
-    --label mp_20_noNumAtomOpt \
-    --num_gradient_steps 5000 \
-    --num_atom_lambda 0 \
-    --composition_lambda 0.1 \
-    --l1_loss
-
-python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/hydra/singlerun/2024-03-12/mp_20 \
-    --num_starting_points 150 \
-    --lr 0.1 \
-    --min_lr 0.001 \
-    --l2_penalty 2e-3 \
-    --num_tested_materials 20 \
-    --label mp_20_l2penalty2e-3 \
+    --label kldBeta_0_05_mp_20_l2_1e-3 \
     --num_gradient_steps 5000 \
     --num_atom_lambda 0.1 \
     --composition_lambda 0.1 \
     --l1_loss
 
 python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/hydra/singlerun/2024-03-12/mp_20 \
+    --model_path /home/gabeguo/hydra/singlerun/2024-03-17/mp_20_vaeBeta05_lr5e-4 \
     --num_starting_points 150 \
     --lr 0.1 \
     --min_lr 0.001 \
     --l2_penalty 5e-5 \
     --num_tested_materials 20 \
-    --label mp_20_noCompositionInfo \
+    --label kldBeta_0_05_mp_20_l2_noFormula \
     --num_gradient_steps 5000 \
     --num_atom_lambda 0 \
     --composition_lambda 0 \
     --l1_loss
+
