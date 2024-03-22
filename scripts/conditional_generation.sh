0fcf256fc1f36956f62843673ffd33da4c9b055c
Gabe Guo <gzg2104@columbia.edu>
2024-03-21 21:27:03 -0400

    conditional generation shell script

--- scripts/conditional_generation.sh
+++ scripts/conditional_generation.sh
@@ -1,39 +1,14 @@
 python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/hydra/singlerun/2024-03-17/mp_20_vaeBeta05_lr5e-4 \
-    --num_starting_points 150 \
+    --model_path /home/gabeguo/hydra/singlerun/2024-03-18/mp20_sharpXRD_L1 \
+    --num_starting_points 200 \
+    --num_candidates 10 \
     --lr 0.1 \
     --min_lr 0.001 \
-    --l2_penalty 5e-5 \
-    --num_tested_materials 20 \
-    --label kldBeta_0_05_mp_20_l2_5e-5 \
+    --l2_penalty 1e-4 \
+    --num_tested_materials 50 \
+    --label dummy \
     --num_gradient_steps 5000 \
     --num_atom_lambda 0.1 \
     --composition_lambda 0.1 \
-    --l1_loss
-
-python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/hydra/singlerun/2024-03-17/mp_20_vaeBeta05_lr5e-4 \
-    --num_starting_points 150 \
-    --lr 0.1 \
-    --min_lr 0.001 \
-    --l2_penalty 1e-3 \
-    --num_tested_materials 20 \
-    --label kldBeta_0_05_mp_20_l2_1e-3 \
-    --num_gradient_steps 5000 \
-    --num_atom_lambda 0.1 \
-    --composition_lambda 0.1 \
-    --l1_loss
-
-python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/hydra/singlerun/2024-03-17/mp_20_vaeBeta05_lr5e-4 \
-    --num_starting_points 150 \
-    --lr 0.1 \
-    --min_lr 0.001 \
-    --l2_penalty 5e-5 \
-    --num_tested_materials 20 \
-    --label kldBeta_0_05_mp_20_l2_noFormula \
-    --num_gradient_steps 5000 \
-    --num_atom_lambda 0 \
-    --composition_lambda 0 \
-    --l1_loss
-
+    --l1_loss \
+    --n_step_each 100
