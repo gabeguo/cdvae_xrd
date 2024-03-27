0871c21872e17e3a2b158a6b14361b8244af34b9
Gabe Guo <gzg2104@columbia.edu>
2024-03-27 15:29:45 -0400

    removed deprecated classes

--- cdvae/pl_modules/xrd.py
+++ cdvae/pl_modules/xrd.py
@@ -2,37 +2,6 @@ import torch
 from torch import nn
 import numpy as np
 
-class XRDEncoder(nn.Module):
-    def __init__(self, num_channels=1):
-        super(XRDEncoder, self).__init__()
-
-        self.num_channels = num_channels
-        layer_list = [
-            nn.Sequential(
-                nn.Conv1d(in_channels=self.num_channels, out_channels=64, kernel_size=3, padding=1, bias=False),
-                nn.BatchNorm1d(num_features=64),
-                nn.ReLU()
-            )
-        ]
-        curr_num_channels = 64
-        for _ in range(3):
-            # downsize to half the previous size
-            layer_list.append(nn.AvgPool1d(kernel_size=2, stride=2, padding=0))
-            for i in range(2):
-                layer_list.append(nn.Sequential(
-                    nn.Conv1d(in_channels=curr_num_channels, out_channels=curr_num_channels//2, kernel_size=3, padding=1, bias=False),
-                    nn.BatchNorm1d(num_features=curr_num_channels//2),
-                    nn.ReLU()
-                ))
-                curr_num_channels = curr_num_channels // 2
-        self.layers = nn.Sequential(*layer_list)
-        self.linear = nn.Linear(in_features=64, out_features=256)
-
-    def forward(self, x):
-        result = self.layers(x)
-        result = self.linear(result.reshape(x.size(0), -1))
-        return result
-
 class XRDDenseRegressor(nn.Module):
     def __init__(self, latent_dim=256, xrd_dim=512, num_blocks=4):
         super(XRDDenseRegressor, self).__init__()
@@ -108,6 +77,7 @@ class XRDDenseRegressor(nn.Module):
 
 class XRDConvRegressor(nn.Module):
     def __init__(self, latent_dim=256, xrd_dim=512):
+        raise ValueError('conv regressor is deprecated')
         super(XRDConvRegressor, self).__init__()
 
         self.high_dim_proj = nn.Linear(latent_dim, xrd_dim)
