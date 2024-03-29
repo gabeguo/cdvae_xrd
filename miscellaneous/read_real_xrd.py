66fe93443265400b5b1d2530ee9c702d9549615e
Gabe Guo <gzg2104@columbia.edu>
2024-03-29 11:31:50 -0400

    made it parse XRDs in real space

--- miscellaneous/read_real_xrd.py
+++ miscellaneous/read_real_xrd.py
@@ -1,7 +1,19 @@
 import torch
+import numpy as np
 import argparse
 import os
 import matplotlib.pyplot as plt
+from pymatgen.analysis.diffraction.xrd import XRDCalculator, WAVELENGTHS
+
+def get_field_value(all_lines, desired_start):
+    for i, the_line in enumerate(all_lines):
+        if the_line.startswith(desired_start):
+            split_line = the_line.split()
+            if len(split_line) > 1:
+                return split_line[-1]
+            else:
+                all_lines[i+1]
+    return None
 
 def main(args):
     assert os.path.exists(args.filepath)
@@ -11,7 +23,7 @@ def main(args):
         assert xrd_loop_start_idx >= 0
         expected_fields = ['_pd_meas_intensity_total',
                                     '_pd_proc_ls_weight',
-                                    '_pd_proc_intensity_calc_bkg',
+                                    '_pd_proc_intensity_bkg_calc',
                                     '_pd_calc_intensity_total']
         for idx in range(len(expected_fields)):
             assert expected_fields[idx] == all_lines[xrd_loop_start_idx + (idx + 1)]
@@ -21,32 +33,46 @@ def main(args):
 
         xrd_tensor = torch.zeros(args.xrd_dim)
 
+        _2theta_min_deg = float(get_field_value(all_lines, '_pd_meas_2theta_range_min'))
+        _2theta_max_deg = float(get_field_value(all_lines, '_pd_meas_2theta_range_max'))
+        _wavelength = float(get_field_value(all_lines, '_diffrn_radiation_wavelength'))
+
+        print(_2theta_min_deg)
+        print(_2theta_max_deg)
+        print(_wavelength)
+
+        theta_min_rad = (_2theta_min_deg / 2) * np.pi / 180
+        theta_max_rad = (_2theta_max_deg / 2) * np.pi / 180
+
         xrd_intensities = all_lines[start_idx:end_idx]
-        for idx, xrd_info in enumerate(xrd_intensities):
+        the_thetas = np.linspace(theta_min_rad, theta_max_rad, len(xrd_intensities))
+
+        for i in range(len(the_thetas)):
+            curr_theta = the_thetas[i]
+            xrd_info = xrd_intensities[i]
             xrd_info = xrd_info.split()
             assert len(xrd_info) == len(expected_fields)
-            assert xrd_info[0].format
             intensity_mean, intensity_std = xrd_info[0].split('(')
             intensity_std = float(intensity_std[:-1])
             intensity_mean = float(intensity_mean)
+            intensity_mean, intensity_std = xrd_info[0].split('(')
+            intensity_std = float(intensity_std[:-1])
+            intensity_mean = float(intensity_mean)
+
+            q = 4 * np.pi * np.sin(curr_theta) / _wavelength
+            if q  > args.max_Q:
+                print(f'{q} exceeds max q {args.max_Q}: {i}/{len(the_thetas)}')
+                break
             
-            closest_tensor_idx = int(idx / len(xrd_intensities) * args.xrd_dim)
+            closest_tensor_idx = int((q - args.min_Q) / (args.max_Q - args.min_Q) * args.xrd_dim)
             xrd_tensor[closest_tensor_idx] = max(xrd_tensor[closest_tensor_idx],
-                                                 intensity_mean)
-                                                #torch.normal(mean=intensity_mean, std=intensity_std, size=(1,)).item())
-        
-        print('moo')
+                                                 intensity_mean)            
+
         #print(xrd_tensor)
         xrd_tensor = (xrd_tensor - torch.min(xrd_tensor)) / (torch.max(xrd_tensor) - torch.min(xrd_tensor))
-        plt.plot([_ for _ in range(args.xrd_dim)], xrd_tensor.detach().cpu().numpy())
+        plt.plot(np.linspace(args.min_Q, args.max_Q, len(xrd_tensor)), xrd_tensor.detach().cpu().numpy())
         filename = args.filepath.split('/')[-1]
         plt.savefig(f'{filename}.png')
-
-        # print out diffraction pattern info
-        radiation_info_start = all_lines.index('_reflns_number_observed', end_idx)
-        radiation_info_end = all_lines.index('', radiation_info_start)
-        for idx in range(radiation_info_start, radiation_info_end):
-            print(all_lines[idx])
 
     return
 
@@ -56,15 +82,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--filepath',
                         type=str,
-                        default='/home/gabeguo/experimental_cif/an0607ARZO20A_p_01sup3.rtv.combined.cif')
+                        default='/home/gabeguo/experimental_cif/zm5036Isup2.rtv.combined.cif')
     parser.add_argument('--xrd_dim',
                         type=int,
-                        default=512)
-    parser.add_argument('--min_theta',
+                        default=4096)
+    parser.add_argument('--min_Q',
                         type=int,
                         default=0)
-    parser.add_argument('--max_theta',
+    parser.add_argument('--max_Q',
                         type=int,
-                        default=180)
+                        default=10)
     args = parser.parse_args()
     main(args)
