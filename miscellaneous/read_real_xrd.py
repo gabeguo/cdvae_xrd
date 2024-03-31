6b44cc06e2637bb2bb3579e9544ca36b72909dea
TristanSaidi <tls2160@columbia.edu>
2024-03-31 14:07:19 +0000

    uncommitted stuff from previous merge conflict

--- miscellaneous/read_real_xrd.py
+++ miscellaneous/read_real_xrd.py
@@ -1,52 +1,121 @@
 import torch
+import numpy as np
 import argparse
 import os
 import matplotlib.pyplot as plt
+from pymatgen.analysis.diffraction.xrd import XRDCalculator, WAVELENGTHS
+from pymatgen.core.structure import Structure
+from scripts.gen_xrd import create_xrd_tensor
+
+# Works on zm5036, wm6137, wm2699
+
+def get_field_value(all_lines, desired_start):
+    for i, the_line in enumerate(all_lines):
+        if the_line.startswith(desired_start):
+            split_line = the_line.split()
+            if len(split_line) > 1:
+                return split_line[-1]
+            else:
+                ret_val = all_lines[i+1]
+                tokens = ret_val.split()
+                for the_token in tokens:
+                    try:
+                        return float(the_token)
+                    except ValueError:
+                        continue
+                raise ValueError(f'invalid field value for {desired_start}')
+    raise ValueError(f'could not find field {desired_start}')
 
 def main(args):
+    sim_wavelength = WAVELENGTHS[args.desired_wavelength]
     assert os.path.exists(args.filepath)
     with open(args.filepath, 'r') as fin:
         all_lines = [x.strip() for x in fin.readlines()]
         xrd_loop_start_idx = all_lines.index('loop_')
         assert xrd_loop_start_idx >= 0
-        expected_fields = ['_pd_meas_intensity_total',
-                                    '_pd_proc_ls_weight',
-                                    '_pd_proc_intensity_calc_bkg',
-                                    '_pd_calc_intensity_total']
+        if 'zm5036' in args.filepath:
+            expected_fields = ['_pd_meas_intensity_total', # '_pd_meas_intensity_total'
+                            '_pd_proc_ls_weight',
+                            '_pd_proc_intensity_bkg_calc',
+                            '_pd_calc_intensity_total']
+        elif 'wm6137' in args.filepath:
+            expected_fields = ['_pd_meas_counts_total',
+                            '_pd_proc_ls_weight',
+                            '_pd_proc_intensity_bkg_calc',
+                            '_pd_calc_intensity_total']  
+        elif 'wm2699' in args.filepath:
+            expected_fields = ['_pd_meas_counts_total', '_pd_calc_intensity_total']
         for idx in range(len(expected_fields)):
             assert expected_fields[idx] == all_lines[xrd_loop_start_idx + (idx + 1)]
-        start_idx = xrd_loop_start_idx + (4+1)
+        for idx in range(xrd_loop_start_idx + (len(expected_fields)+1), len(all_lines)):
+            if all_lines[idx].strip() != '':
+                start_idx = idx
+                break
+        
         end_idx = all_lines.index('', start_idx)
         assert end_idx > start_idx
 
-        xrd_tensor = torch.zeros(args.xrd_dim)
+        _2theta_min_deg = float(get_field_value(all_lines, '_pd_meas_2theta_range_min'))
+        _2theta_max_deg = float(get_field_value(all_lines, '_pd_meas_2theta_range_max'))
+        _exp_wavelength = float(get_field_value(all_lines, '_diffrn_radiation_wavelength'))
+
+        print(_2theta_min_deg)
+        print(_2theta_max_deg)
+        print(_exp_wavelength)
+
+        theta_min_rad = (_2theta_min_deg / 2) * np.pi / 180
+        theta_max_rad = (_2theta_max_deg / 2) * np.pi / 180
 
         xrd_intensities = all_lines[start_idx:end_idx]
-        for idx, xrd_info in enumerate(xrd_intensities):
-            xrd_info = xrd_info.split()
-            assert len(xrd_info) == len(expected_fields)
-            assert xrd_info[0].format
-            intensity_mean, intensity_std = xrd_info[0].split('(')
-            intensity_std = float(intensity_std[:-1])
-            intensity_mean = float(intensity_mean)
-            
-            closest_tensor_idx = int(idx / len(xrd_intensities) * args.xrd_dim)
-            xrd_tensor[closest_tensor_idx] = max(xrd_tensor[closest_tensor_idx],
-                                                 intensity_mean)
-                                                #torch.normal(mean=intensity_mean, std=intensity_std, size=(1,)).item())
+        the_thetas_rad = np.linspace(theta_min_rad, theta_max_rad, len(xrd_intensities))
+        print('theta range (rad):', np.min(the_thetas_rad), np.max(the_thetas_rad))
+        converted_2thetas = 2 * np.arcsin(np.sin(the_thetas_rad) * sim_wavelength / _exp_wavelength) * 180 / np.pi
+        print('2theta range (deg):', np.nanmin(converted_2thetas), np.nanmax(converted_2thetas))
         
-        print('moo')
+        xrd_tensor = torch.zeros(args.xrd_vector_dim)
+
+        _2thetas = np.linspace(args.min_2theta, args.max_2theta, args.xrd_vector_dim)
+
+        for i in range(len(converted_2thetas)):
+            curr_2theta = converted_2thetas[i]
+            xrd_info = xrd_intensities[i]
+            xrd_info = xrd_info.split()
+            #print(xrd_info, expected_fields)
+            if len(xrd_info) != len(expected_fields):
+                break
+            try:
+                intensity_mean = float(xrd_info[0])
+            except ValueError:
+                intensity_mean, intensity_std = xrd_info[0].split('(')
+                intensity_std = float(intensity_std[:-1])
+                intensity_mean = float(intensity_mean)
+
+            if np.isnan(curr_2theta):
+                print(f'{curr_2theta} too big: {i} out of {len(converted_2thetas)}')
+                break
+            
+            closest_tensor_idx = int((curr_2theta - args.min_2theta) / (args.max_2theta - args.min_2theta) * args.xrd_vector_dim)
+            xrd_tensor[closest_tensor_idx] = max(xrd_tensor[closest_tensor_idx],
+                                                 intensity_mean)   
+
         #print(xrd_tensor)
         xrd_tensor = (xrd_tensor - torch.min(xrd_tensor)) / (torch.max(xrd_tensor) - torch.min(xrd_tensor))
-        plt.plot([_ for _ in range(args.xrd_dim)], xrd_tensor.detach().cpu().numpy())
+        plt.plot(_2thetas, xrd_tensor.detach().cpu().numpy(), alpha=0.6, label='converted')
         filename = args.filepath.split('/')[-1]
-        plt.savefig(f'{filename}.png')
-
-        # print out diffraction pattern info
-        radiation_info_start = all_lines.index('_reflns_number_observed', end_idx)
-        radiation_info_end = all_lines.index('', radiation_info_start)
-        for idx in range(radiation_info_start, radiation_info_end):
-            print(all_lines[idx])
+    
+    # TODO: generate GT XRD for sanity check
+    structure = Structure.from_file(args.filepath)
+    # wavelength
+    curr_wavelength = WAVELENGTHS[args.desired_wavelength]
+    # Create the XRD calculator
+    xrd_calc = XRDCalculator(wavelength=curr_wavelength)
+    # Calculate the XRD pattern
+    pattern = xrd_calc.get_pattern(structure)
+    simulated_xrd_tensor = create_xrd_tensor(args, pattern)
+    plt.plot(_2thetas, simulated_xrd_tensor.detach().cpu().numpy(), alpha=0.6, label='simulated')
+    
+    plt.legend()
+    plt.savefig(f'{filename}.png')
 
     return
 
@@ -56,15 +125,24 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--filepath',
                         type=str,
-                        default='/home/gabeguo/experimental_cif/an0607ARZO20A_p_01sup3.rtv.combined.cif')
-    parser.add_argument('--xrd_dim',
+                        #default='/home/gabeguo/experimental_cif/wm2699Isup2.rtv.combined.cif')
+                        #default='/home/gabeguo/experimental_cif/wm6137Isup2.rtv.combined.cif')
+                        default='/home/gabeguo/experimental_cif/zm5036Isup2.rtv.combined.cif')
+    parser.add_argument('--desired_wavelength',
+                        type=str,
+                        default='CuKa')
+    parser.add_argument('--xrd_vector_dim',
                         type=int,
-                        default=512)
-    parser.add_argument('--min_theta',
+                        default=4096)
+    parser.add_argument('--min_2theta',
                         type=int,
                         default=0)
-    parser.add_argument('--max_theta',
+    parser.add_argument('--max_2theta',
                         type=int,
                         default=180)
     args = parser.parse_args()
+
+    setattr(args, 'min_theta', args.min_2theta)
+    setattr(args, 'max_theta', args.max_2theta)
+
     main(args)
