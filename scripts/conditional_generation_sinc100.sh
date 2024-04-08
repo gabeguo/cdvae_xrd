9cfeb2b48d9ca7888ab4f49cf6b9fc363482d9d1
Gabe Guo <gzg2104@columbia.edu>
2024-04-08 11:26:21 -0400

    Correct version of conditional generation

--- scripts/conditional_generation_sinc100.sh
+++ scripts/conditional_generation_sinc100.sh
@@ -1,14 +1,15 @@
 python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/hydra/singlerun/2024-04-03/mp_20_sincSmooth100 \
+    --model_path /home/gabeguo/hydra/singlerun/2024-04-07/mp_20_sincSmooth100 \
     --num_starting_points 200 \
     --num_candidates 5 \
     --lr 0.1 \
     --min_lr 1e-4 \
     --l2_penalty 2e-4 \
     --num_tested_materials 200 \
-    --label _sinc100_correctFilter \
+    --label _sinc100_ \
     --num_gradient_steps 5000 \
     --num_atom_lambda 0.1 \
     --n_step_each 100 \
     --composition_lambda 0.1 \
     --l1_loss \
+    --output_dir paper_results
