cbf514a3a73efc2e8ef17ff7a4275f77e50a595d
Gabe Guo <gzg2104@columbia.edu>
2024-04-15 21:11:37 -0400

    Updated output dir

--- scripts/eval_xrd.sh
+++ scripts/eval_xrd.sh
@@ -1,4 +1,4 @@
 python -W ignore eval_xrd.py \
     --model_path /home/gabeguo/hydra/singlerun/2024-04-13/mp_20_directXRDCondition_beta1e-3_lr1e-3 \
-    --output_dir dummy_eval_xrd_output \
+    --output_dir prelim_directXRDCondition_output \
     --n_step_each 100
