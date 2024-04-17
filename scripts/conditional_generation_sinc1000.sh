8bcc347dfc63dc74ff5e238cfe39bda69ca9af5b
TristanSaidi <tls2160@columbia.edu>
2024-04-17 19:48:04 +0000

    changing label

--- scripts/conditional_generation_sinc1000.sh
+++ scripts/conditional_generation_sinc1000.sh
@@ -1,12 +1,12 @@
 python -W ignore conditional_generation.py \
-    --model_path /home/tsaidi/Research/cdvae_xrd/hydra/singlerun/2024-04-12/mp_20_sinc_filt_gauss_filt_nanomaterial_size_1000_sga \
+    --model_path /home/tsaidi/Research/cdvae_xrd/hydra/singlerun/2024-04-12/mp_20_sinc_filt_gauss_filt_nanomaterial_size_1000 \
     --num_starting_points 100 \
     --num_candidates 5 \
     --lr 0.1 \
     --min_lr 1e-4 \
     --l2_penalty 2e-4 \
     --num_tested_materials 200 \
-    --label _sinc100_ \
+    --label _sinc1000_ \
     --num_gradient_steps 5000 \
     --num_atom_lambda 0.1 \
     --n_step_each 100 \
