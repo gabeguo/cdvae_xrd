461f2417310dd4bdf5eacfdc5b2df09f434b8c00
Gabe Guo <gzg2104@columbia.edu>
2024-04-24 10:20:40 -0400

    Updated script to train PDF model

--- scripts/train_mp20_model_pdf.sh
+++ scripts/train_mp20_model_pdf.sh
@@ -1,2 +1,2 @@
 cd ../
-python -W ignore cdvae/run.py data=mp_20_pdf expname=mp_20_pdf model.predict_property=True
+python -W ignore cdvae/run.py data=mp_20_pdf_sinc100 expname=mp_20_pdf_sinc100 model.predict_property=True
