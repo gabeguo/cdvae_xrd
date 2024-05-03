2fb5706b7f78663c9b124795d4212f2fcf64abe6
Gabe Guo <gzg2104@columbia.edu>
2024-05-03 16:42:57 -0400

    script that gets results by crystal system

--- scripts/extract_results_by_crystal_system.py
+++ scripts/extract_results_by_crystal_system.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 
 MATCH_RATE = "match_rate"
 PEARSON_R = "Mean best Pearson's correlation coefficient between PDFs"
+COUNT = 'count'
 
 def is_match(the_dict):
     all_match_statuses = the_dict[MATCH_RATE]
@@ -30,13 +31,16 @@ def update_curr_system_dict(aggregate_results, the_system, the_correlation,
     if the_system not in aggregate_results:
         aggregate_results[the_system] = {PEARSON_R: list(),
                                          MATCH_RATE: list()}
-    aggregate_results[the_system][PEARSON_R].append(the_correlation)
-    aggregate_results[the_system][MATCH_RATE].append(match_status)
+    aggregate_results[the_system][PEARSON_R].append(float(the_correlation))
+    aggregate_results[the_system][MATCH_RATE].append(float(match_status))
 
     return
 
 def postprocess(aggregate_results):
-    for system_results in aggregate_results:
+    for crystal_system in aggregate_results:
+        system_results = aggregate_results[crystal_system]
+        system_results[COUNT] = len(system_results[MATCH_RATE])
+        assert system_results[COUNT] == len(system_results[PEARSON_R])
         system_results[PEARSON_R] = (round(np.mean(system_results[PEARSON_R]), 4),
                                      round(np.std(system_results[PEARSON_R]), 4))
         system_results[MATCH_RATE] = np.mean(system_results[MATCH_RATE])
@@ -64,13 +68,21 @@ def main(args):
                                 match_status=match_status)
     
     postprocess(aggregate_results)
-    print(json.dumps(aggregate_results), indent=4)
+    print(json.dumps(aggregate_results, indent=4))
+
+    os.makedirs(args.output_dir, exist_ok=True)
+    experiment_name = args.results_dir.split('/')[-1]
+    output_filepath = os.path.join(args.output_dir, f"{experiment_name}.json")
+    with open(output_filepath, 'w') as fout:
+        json.dump(aggregate_results, fout, indent=4)
 
     return
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--results_dir', type=str)
+    parser.add_argument('--output_dir', type=str, default='results_by_crystal_system')
     args = parser.parse_args()
 
+    print('results for:', args.results_dir)
     main(args)
---
+++ scripts/extract_results_by_crystal_system.sh
@@ -0,0 +1,6 @@
+python -W ignore extract_results_by_crystal_system.py \
+    --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc100_
+python -W ignore extract_results_by_crystal_system.py \
+    --results_dir //home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc10_replicate
+python -W ignore extract_results_by_crystal_system.py \
+    --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/experimental_sinc10Filter_
