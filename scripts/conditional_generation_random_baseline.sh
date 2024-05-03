6e6446c3738fa1e05f0c8b7cd8b90e3cab4dc5d9
Gabe Guo <gzg2104@columbia.edu>
2024-05-03 00:18:16 -0400

    add random baseline

---
+++ scripts/conditional_generation_random_baseline.sh
@@ -0,0 +1,17 @@
+python -W ignore conditional_generation.py \
+    --model_path /home/gabeguo/hydra/singlerun/2024-04-07/mp_20_sincSmooth10 \
+    --num_starting_points 100 \
+    --num_candidates 5 \
+    --lr 0 \
+    --min_lr 0 \
+    --l2_penalty 0 \
+    --num_tested_materials 200 \
+    --label _random_baseline_ \
+    --num_gradient_steps 0 \
+    --num_atom_lambda 0 \
+    --n_step_each 1 \
+    --composition_lambda 0 \
+    --l1_loss \
+    --r_min 0 \
+    --r_max 30 \
+    --output_dir paper_results_PRELIM
