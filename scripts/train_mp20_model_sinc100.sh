7545d0b524bb29e5b906cb9ecaa54de06c0440e6
TristanSaidi <tls2160@columbia.edu>
2024-05-15 17:13:59 +0000

    incorrect name for data dir -- fixing it now

--- scripts/train_mp20_model_sinc100.sh
+++ scripts/train_mp20_model_sinc100.sh
@@ -1,2 +1,2 @@
 cd ../
-python -W ignore cdvae/run.py data=mp_20_sinc100 expname=mp_20_sinc100 model.predict_property=True
+python -W ignore cdvae/run.py data=mp_20 expname=mp_20_sinc100 model.predict_property=True
