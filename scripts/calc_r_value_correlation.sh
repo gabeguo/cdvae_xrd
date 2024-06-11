54a54e37e4a7dbf1385c2ddb8d82e3ff9303cedb
Gabe Guo <gzg2104@columbia.edu>
2024-06-11 14:07:45 -0400

    added including outliers

--- scripts/calc_r_value_correlation.sh
+++ scripts/calc_r_value_correlation.sh
@@ -33,3 +33,12 @@ python calculate_r_value_correlation.py \
     --sinc 100 \
     --thresh_x 1.4 \
     --thresh_y 1.4
+
+python calculate_r_value_correlation.py \
+    --unrefined_json /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc100_/r_values.json \
+    --refined_txt /home/gabeguo/refined_candidates_05_16_24_rVal/fitted/pred_100/sinc100_and_gaussian_unitCellRefine/Rw_values.txt \
+    --save_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/05_16_24_rw_refinement/sinc100/lattice_refinement_NO_OUTLIERS \
+    --curated_candidates_folder /home/gabeguo/cdvae_xrd/Data_to_Max_05_07_24 \
+    --sinc 100 \
+    --thresh_x 2.5 \
+    --thresh_y 2.5
