03f49995e076fd018234598c292d618a17999810
Gabe Guo <gzg2104@columbia.edu>
2024-06-11 22:30:42 -0400

    allows us to calculate with the outlier too

--- scripts/calc_r_value_comparison.sh
+++ scripts/calc_r_value_comparison.sh
@@ -9,3 +9,10 @@ python calculate_r_factor_comparison.py \
     --refined_input_directory /home/gabeguo/refined_candidates_05_16_24_rVal/fitted/pred_100/sinc100_and_gaussian_unitCellRefine \
     --output_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/refined_candidates_05_16_24_rVal/xrd_comparison/sinc100 \
     --sinc_level 100
+
+python calculate_r_factor_comparison.py \
+    --unrefined_input_directory /home/gabeguo/cdvae_xrd/Data_to_Max_05_07_24 \
+    --refined_input_directory /home/gabeguo/refined_candidates_05_16_24_rVal/fitted/pred_100/sinc100_and_gaussian_unitCellRefine \
+    --output_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/refined_candidates_05_16_24_rVal/xrd_comparison/sinc100_WITH_OUTLIER \
+    --sinc_level 100 \
+    --thresh 2.5
