4e7db383c60c359787cf7f70b4ea22db9b01ac98
Gabe Guo <gzg2104@columbia.edu>
2024-06-11 22:00:15 -0400

    Updated r value correlation script

--- scripts/calc_r_value_correlation.sh
+++ scripts/calc_r_value_correlation.sh
@@ -1,15 +1,6 @@
 python calculate_r_value_correlation.py \
     --unrefined_json /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc10_replicate/r_values.json \
-    --refined_txt /home/gabeguo/refined_candidates_05_16_24_rVal/fitted/pred_10/sinc10_and_gaussian/Rw_values.txt \
-    --save_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/05_16_24_rw_refinement/sinc10/atomic_refinement \
-    --curated_candidates_folder /home/gabeguo/cdvae_xrd/Data_to_Max_05_07_24 \
-    --sinc 10 \
-    --thresh_x 1.4 \
-    --thresh_y 1.4
-
-python calculate_r_value_correlation.py \
-    --unrefined_json /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc10_replicate/r_values.json \
-    --refined_txt /home/gabeguo/refined_candidates_05_16_24_rVal/fitted/pred_10/sinc10_and_gaussian_refineUnitCell/Rw_values.txt \
+    --refined_txt /home/gabeguo/refined_candidates_05_16_24_rVal/deprecated/xrd_comparison/sinc10/Rw_values.txt \
     --save_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/05_16_24_rw_refinement/sinc10/lattice_refinement \
     --curated_candidates_folder /home/gabeguo/cdvae_xrd/Data_to_Max_05_07_24 \
     --sinc 10 \
@@ -18,16 +9,7 @@ python calculate_r_value_correlation.py \
 
 python calculate_r_value_correlation.py \
     --unrefined_json /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc100_/r_values.json \
-    --refined_txt /home/gabeguo/refined_candidates_05_16_24_rVal/fitted/pred_100/sinc100_and_gaussian/Rw_values.txt \
-    --save_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/05_16_24_rw_refinement/sinc100/atomic_refinement \
-    --curated_candidates_folder /home/gabeguo/cdvae_xrd/Data_to_Max_05_07_24 \
-    --sinc 100 \
-    --thresh_x 1.4 \
-    --thresh_y 1.4
-
-python calculate_r_value_correlation.py \
-    --unrefined_json /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc100_/r_values.json \
-    --refined_txt /home/gabeguo/refined_candidates_05_16_24_rVal/fitted/pred_100/sinc100_and_gaussian_unitCellRefine/Rw_values.txt \
+    --refined_txt /home/gabeguo/refined_candidates_05_16_24_rVal/deprecated/xrd_comparison/sinc100/Rw_values.txt \
     --save_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/05_16_24_rw_refinement/sinc100/lattice_refinement \
     --curated_candidates_folder /home/gabeguo/cdvae_xrd/Data_to_Max_05_07_24 \
     --sinc 100 \
@@ -36,7 +18,7 @@ python calculate_r_value_correlation.py \
 
 python calculate_r_value_correlation.py \
     --unrefined_json /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc100_/r_values.json \
-    --refined_txt /home/gabeguo/refined_candidates_05_16_24_rVal/fitted/pred_100/sinc100_and_gaussian_unitCellRefine/Rw_values.txt \
+    --refined_txt /home/gabeguo/refined_candidates_05_16_24_rVal/deprecated/xrd_comparison/sinc100/Rw_values.txt \
     --save_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/05_16_24_rw_refinement/sinc100/lattice_refinement_NO_OUTLIERS \
     --curated_candidates_folder /home/gabeguo/cdvae_xrd/Data_to_Max_05_07_24 \
     --sinc 100 \
