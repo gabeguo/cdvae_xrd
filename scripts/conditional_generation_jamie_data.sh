4058ebf2cd08c52df8f7c2c2d0c932415536b814
Gabe Guo <gzg2104@columbia.edu>
2024-10-14 14:29:28 -0700

    slightly decreased computational burden

--- scripts/conditional_generation_jamie_data.sh
+++ scripts/conditional_generation_jamie_data.sh
@@ -1,7 +1,7 @@
 python -W ignore conditional_generation.py \
-    --model_path /home/gabeguo/cdvae_xrd/outputs/singlerun/2024-09-11/mp_20_sinc100_REAL_DATA \
-    --num_starting_points 200 \
-    --num_candidates 20 \
+    --model_path /home/gabeguo/cdvae_xrd/outputs/singlerun/2024-09-11/mp_20_sinc100_REAL_DATA_noSinc \
+    --num_starting_points 150 \
+    --num_candidates 10 \
     --lr 0.1 \
     --min_lr 1e-4 \
     --l2_penalty 2e-4 \
