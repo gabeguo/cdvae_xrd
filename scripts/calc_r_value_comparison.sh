d4b62fb9332fa49b4699aa0f33e713fef4639fdf
Gabe Guo <gzg2104@columbia.edu>
2024-10-16 14:47:08 -0700

    changed file paths

--- scripts/calc_r_value_comparison.sh
+++ scripts/calc_r_value_comparison.sh
@@ -1,18 +1,11 @@
 python calculate_r_factor_comparison.py \
-    --unrefined_input_directory /home/gabeguo/cdvae_xrd/Data_to_Max_05_07_24 \
-    --refined_input_directory /home/gabeguo/refined_candidates_05_16_24_rVal/fitted/pred_10/sinc10_and_gaussian_refineUnitCell \
-    --output_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/refined_candidates_05_16_24_rVal/xrd_comparison/sinc10 \
+    --unrefined_input_directory /home/gabeguo/cdvae_xrd/candidates_to_max_09_20_24/_sincSq10_ \
+    --refined_input_directory /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_MAX_REFINED/make_sincSq10_ \
+    --output_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_MAX_REFINED/xrd_comparison/sinc10 \
     --sinc_level 10
 
 python calculate_r_factor_comparison.py \
-    --unrefined_input_directory /home/gabeguo/cdvae_xrd/Data_to_Max_05_07_24 \
-    --refined_input_directory /home/gabeguo/refined_candidates_05_16_24_rVal/fitted/pred_100/sinc100_and_gaussian_unitCellRefine \
-    --output_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/refined_candidates_05_16_24_rVal/xrd_comparison/sinc100 \
+    --unrefined_input_directory /home/gabeguo/cdvae_xrd/candidates_to_max_09_20_24/_sincSq100_ \
+    --refined_input_directory /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_MAX_REFINED/make_sincSq100_ \
+    --output_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_MAX_REFINED/xrd_comparison/sinc100 \
     --sinc_level 100
-
-python calculate_r_factor_comparison.py \
-    --unrefined_input_directory /home/gabeguo/cdvae_xrd/Data_to_Max_05_07_24 \
-    --refined_input_directory /home/gabeguo/refined_candidates_05_16_24_rVal/fitted/pred_100/sinc100_and_gaussian_unitCellRefine \
-    --output_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/refined_candidates_05_16_24_rVal/xrd_comparison/sinc100_WITH_OUTLIER \
-    --sinc_level 100 \
-    --thresh 2.5
