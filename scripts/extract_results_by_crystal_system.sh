570efc885b83f0ac514e45aadb45a32eda699f32
Gabe Guo <gzg2104@columbia.edu>
2024-10-19 17:09:53 -0700

    redo script to extract result by crystal system

--- scripts/extract_results_by_crystal_system.sh
+++ scripts/extract_results_by_crystal_system.sh
@@ -2,19 +2,13 @@ python -W ignore extract_results_by_crystal_system.py \
     --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc100_
 python -W ignore extract_results_by_crystal_system.py \
     --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc100_baseline_noOpt
-python -W ignore extract_results_by_crystal_system.py \
-    --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_random_baseline_sinc100_
 
 python -W ignore extract_results_by_crystal_system.py \
-    --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc10_replicate
+    --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc10_
 python -W ignore extract_results_by_crystal_system.py \
     --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_sinc10_baseline_noOpt
-python -W ignore extract_results_by_crystal_system.py \
-    --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_random_baseline_sinc10_
 
 python -W ignore extract_results_by_crystal_system.py \
-    --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/experimental_sinc10Filter_
+    --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/experimental_sinc100Filter_
 python -W ignore extract_results_by_crystal_system.py \
     --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/experimental_baseline_noOpt
-python -W ignore extract_results_by_crystal_system.py \
-    --results_dir /home/gabeguo/cdvae_xrd/paper_results_PRELIM/_random_baseline_EXPERIMENTAL_
